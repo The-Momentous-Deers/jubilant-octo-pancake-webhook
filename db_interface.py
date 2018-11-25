3eea7598427f8bc8b8a92f420fd3e39c53eb1e69
Matt1634 <zewarcobra@gmail.com>
2018-11-25 01:03:34 +0000

    Update ledger_id added

--- db_interface.py
+++ db_interface.py
@@ -11,18 +11,19 @@ class User(Base):
     __tablename__ = 'users'
 
     # We will use the enduser_id as a primary key
-    id = Column(Integer, primary_key=True)
+    id = Column(String, primary_key=True)
     name = Column(String)
     username = Column(String)
     password = Column(String)
+    ledger_id = Column(String)
 
     def __repr__(self):
         return "<User(name='%s', username='%s', password='%s')>" % (
-    self.name, self.fullname, self.password)
+    self.name, self.username, self.password)
 
 
-def addUser(session, enduser_id, enduser_name, enduser_username, enduser_password):
-        toAdd = session.add(User(id= enduser_id, name= enduser_name, username= enduser_username, password=enduser_password))
+def addUser(session, enduser_id, enduser_name, enduser_username, enduser_password, enduser_ledger_id):
+        toAdd = session.add(User(id= enduser_id, name= enduser_name, username= enduser_username, password=enduser_password, ledger_id = enduser_ledger_id))
         session.commit()
 
 def validatePassword(session, nameEntered, passEntered):
@@ -31,3 +32,9 @@ def validatePassword(session, nameEntered, passEntered):
             return enduser_id
         
     return None
+
+def updateLedgerID(session, enduser_id, make_ledger_id):
+    toUpdate = session.query(User).filter_by(id = enduser_id).first()
+    toUpdate.ledger_id = make_ledger_id
+    session.commit()
+
