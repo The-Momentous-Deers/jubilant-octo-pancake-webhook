c7c90607ac22ed9bfde5aa2830afdbbae756c2b1
Jonathan Bartlett <jonathanscott2047@gmail.com>
2018-11-25 01:12:09 +0000

    Add beneficiary capability

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ from flask import make_response
 import json, time, pprint
 from urllib.request import Request, urlopen
 
+from railsbankinterface.railsbankinterface import *
+
 # Flask app should start in global layout
 app = Flask(__name__)
-
+interface = railsbankinterface()
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -31,41 +33,25 @@ def webhook():
 def processRequest(req):
     res = req.get("queryResult")
     act = res.get("action")
+    parameters = req.get("paramters")
     data = None
     if act == "bankbalance":
         print("Hello, Bank Balance PLS!")
-        res = makeWebhookResult(RailsbankRequest().getBalance())
+        res = makeWebhookResult(interface.getBalance())
+    elif act == "beneficiary":
+        print("Hello, Add a Beneficiary PLS!")
+        res = makeWebhookResult(interface.makeBeneficiary(res.get("IBAN"), res.get("BIC"), res.get("given-name")))
     else:
         res = makeWebhookResult("Sorry, Not sure what you mean")    
     return res
 
-base_url = 'https://play.railsbank.com/'
-
-# The next line contains get post and put helper functions, which should be replaced with a library of your choice in production code
-custom_fetch = lambda method, relative_url, body=None: json.loads(urlopen(Request(base_url+relative_url, data=json.dumps(body).encode('utf8'), method=method, headers={'Content-Type': 'application/json', 'Authorization': 'API-Key ' + api_key, 'Accept': 'application/json'})).read().decode('utf-8')); post = lambda url, body=None: custom_fetch("POST", url, body); get = lambda url: custom_fetch("GET", url); put = lambda url, body=None: custom_fetch("PUT", url, body)
-
 # Make sure to replace `***EXAMPLE KEY***` in the next line with your api key of form <key_id>#<key_secret>
 api_key = os.environ.get('API_KEY', None)
-
-class RailsbankRequest:
-
-    ledger_id = "5bf951a0-8c73-437c-ae59-ac60a0f9847e"
-
-    def __init__(self):
-        response = get('v1/customer/me')
-        pprint.pprint(response)
-        self.customer_id = response['customer_id']
-
-    def getBalance(self):
-        response = get('v1/customer/ledgers/' + str(self.ledger_id))
-        return "Your balance is " + str(response['amount']) + " pounds."
-
 
 def makeWebhookResult(speech):
     return {
         "fulfillmentText": speech,
     }
-
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
